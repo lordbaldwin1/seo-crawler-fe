a1d52e812bb22113648d76882e66dd72fdb4f30a
zach <springerzman@gmail.com>
2025-10-25 16:12:29 -0700

    added error handling, loading state

--- src/components/crawler.tsx
+++ src/components/crawler.tsx
@@ -1,11 +1,18 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Header from "./header";
 import { env } from "~/env";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import dynamic from "next/dynamic";
+import { ArrowBigDownIcon, ChevronDown, Loader2 } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "~/components/ui/tooltip"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuRadioGroup, DropdownMenuRadioItem, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
 const ForceGraph3D = dynamic(() => import("react-force-graph-3d"), {
   ssr: false,
 });
@@ -17,7 +24,7 @@ const header = {
 
 export type CrawlURLQueryParameters = {
   url: string;
-  maxPages: number;
+  maxPages: string;
 };
 
 export type ReactForceGraphShape = {
@@ -34,75 +41,126 @@ export default function Crawler() {
   const [showGraph, setShowGraph] = useState<boolean>(false);
   const [queryParams, setQueryParams] = useState<CrawlURLQueryParameters>({
     url: "",
-    maxPages: 50,
+    maxPages: "50",
   });
   const [graphData, setGraphData] = useState<ReactForceGraphShape>({
     nodes: [],
     links: [],
   });
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
+  const inputRef = useRef<HTMLInputElement>(null);
   const GROUPS = 12
 
   async function handleCrawlURL() {
+    setIsLoading(true);
     try {
       const res = await fetch(
         `${env.NEXT_PUBLIC_API_URL}/api/crawl?url=${queryParams.url}&maxPages=${queryParams.maxPages}`,
       );
 
       if (!res.ok) {
-        // ADD ERROR HANDLING
+        const error = (await res.json());
+        setError(`error: ${error.error}`)
         return;
       }
       const data = (await res.json()) as ReactForceGraphShape;
       setGraphData(data);
       setShowGraph(true);
-    } catch {
+    } catch (err) {
+      setError(`${(err as Error).message}`)
     } finally {
-      // ADD LOADING STATE
+      setIsLoading(false);
     }
   }
   return (
-    <section>
+    <section onClick={() => setError("")}>
       {!showGraph ? (
         <div className="flex h-[65vh] flex-col justify-center">
           <Header title={header.title} description={header.description} />
           <section className="w-full space-y-6">
-            <Input
-              placeholder="Enter a URL..."
-              value={queryParams.url}
-              onChange={(e) =>
-                setQueryParams((prevQueryParams) => ({
-                  ...prevQueryParams,
-                  url: e.target.value,
-                }))
-              }
-            />
+            <DropdownMenu>
+              <DropdownMenuTrigger className="outline rounded-sm p-1 px-2 hover:outline-muted-foreground transition-all duration-200 flex flex-row items-center gap-1">max pages: {queryParams.maxPages} <ChevronDown size={18} /></DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel className="sr-only">Panel Position</DropdownMenuLabel>
+                <DropdownMenuRadioGroup value={queryParams.maxPages} onValueChange={(value) => setQueryParams(prevParams => ({
+                  ...prevParams,
+                  maxPages: value
+                }))}>
+                  <DropdownMenuRadioItem value="50">50</DropdownMenuRadioItem>
+                  <DropdownMenuRadioItem value="75">75</DropdownMenuRadioItem>
+                  <DropdownMenuRadioItem value="100">100</DropdownMenuRadioItem>
+                  <DropdownMenuRadioItem value="125">125</DropdownMenuRadioItem>
+                  <DropdownMenuRadioItem value="150">150</DropdownMenuRadioItem>
+                  <DropdownMenuRadioItem value="175">175</DropdownMenuRadioItem>
+                  <DropdownMenuRadioItem value="200">200</DropdownMenuRadioItem>
+                </DropdownMenuRadioGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <Tooltip open={error.length > 0}>
+              <TooltipTrigger asChild>
+                <Input
+                  ref={inputRef}
+                  placeholder="Enter a URL..."
+                  onClick={() => setError("")}
+                  value={queryParams.url}
+                  onChange={(e) =>
+                    setQueryParams((prevQueryParams) => ({
+                      ...prevQueryParams,
+                      url: e.target.value,
+                    }))
+                  }
+                />
+              </TooltipTrigger>
+              <TooltipContent>
+                {error.length > 0 && (
+                  error
+                )}
+              </TooltipContent>
+            </Tooltip>
             <div className="flex w-full items-center justify-center gap-2">
-              <Button className="w-1/2">Clear</Button>
-              <Button className="w-1/2" onClick={handleCrawlURL}>
-                Crawl
+              <Button className="w-1/2" disabled={isLoading ? true : false} onClick={() => {
+                setQueryParams(prevParams => ({
+                  ...prevParams,
+                  url: "",
+                }));
+                inputRef.current?.focus();
+              }}>Clear</Button>
+              <Button className="w-1/2" onClick={handleCrawlURL} disabled={isLoading ? true : false}>
+                {isLoading ? <Loader2 className="animate-spin" /> : "Crawl"}
               </Button>
             </div>
           </section>
         </div>
       ) : (
-        <div className="flex items-center justify-center">
-          <ForceGraph3D
-          graphData={graphData}
-          nodeLabel="id"
-          backgroundColor="rgba(0,0,0,0)"
-          nodeColor={d => `hsl(${(String(d.id).length%GROUPS) * (360/GROUPS)}, 70%, 50%)`}
-          linkColor={d => {
-            const sourceNode = graphData.nodes.find(n => n.id === d.source);
-            const colorIndex = sourceNode ? String(sourceNode.id).length%GROUPS : 0;
-            return `hsl(${colorIndex * (360/GROUPS)}, 70%, 50%)`;
-          }}
-          linkWidth={1}
-          nodeRelSize={2}
-          height={600}
-          width={1200}
-        />
+        <div className="flex flex-col items-center justify-center">
+          <div className="h-[600px] w-[1200px]">
+            <ForceGraph3D
+              graphData={graphData}
+              nodeLabel="id"
+              backgroundColor="rgba(0,0,0,0)"
+              nodeColor={d => `hsl(${(String(d.id).length % GROUPS) * (360 / GROUPS)}, 70%, 50%)`}
+              linkColor={d => {
+                const sourceNode = graphData.nodes.find(n => n.id === d.source);
+                const colorIndex = sourceNode ? String(sourceNode.id).length % GROUPS : 0;
+                return `hsl(${colorIndex * (360 / GROUPS)}, 70%, 50%)`;
+              }}
+              linkWidth={1}
+              nodeRelSize={2}
+              height={600}
+              width={1200}
+            />
+          </div>
+          <Button className="mt-4" onClick={() => {
+            setQueryParams({
+              url: "",
+              maxPages: "50",
+            })
+            setShowGraph(false)
+          }}>test another site</Button>
         </div>
-      )}
-    </section>
+      )
+      }
+    </section >
   );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -10,11 +10,11 @@ import {
   SheetTrigger,
 } from "./ui/sheet";
 import { Menu } from "lucide-react";
+import { Button } from "./ui/button";
 
 export default function Navbar() {
   return (
-    <div className="mb-12 flex flex-row items-center justify-end sm:justify-between gap-4">
-      {/* Desktop Navigation */}
+    <div className="mb-12 flex flex-row items-center justify-between gap-4">
       <div className="hidden sm:flex items flex-wrap gap-4">
         <Link
           href={"/"}
@@ -23,59 +23,13 @@ export default function Navbar() {
           <span>seo-crawler</span>
         </Link>
       </div>
-      
-      {/* Desktop Theme Toggle */}
-      <div className="hidden sm:block">
-        <ThemeToggle />
-      </div>
 
-      {/* Mobile Hamburger Menu */}
-      <div className="sm:hidden flex justify-end">
-        <Sheet>
-          <SheetTrigger asChild className="-mb-4">
-            <button className="hover:text-accent transition-colors">
-              <Menu />
-              <span className="sr-only">Open menu</span>
-            </button>
-          </SheetTrigger>
-          <SheetContent side="right">
-            <SheetHeader>
-              <SheetTitle>navigation</SheetTitle>
-              <SheetDescription>
-                tap the links to navigate.
-              </SheetDescription>
-            </SheetHeader>
-              <div className="flex flex-col gap-6 ml-6">
-                <SheetClose asChild>
-                  <Link
-                    href={"/"}
-                    className="hover:text-accent group flex items-center transition-all duration-200"
-                  >
-                    <span>home</span>
-                  </Link>
-                </SheetClose>
-                <SheetClose asChild>
-                  <Link
-                    href={"/projects"}
-                    className="hover:text-accent group flex items-center transition-all duration-200"
-                  >
-                    <span>projects</span>
-                  </Link>
-                </SheetClose>
-                <SheetClose asChild>
-                  <Link
-                    href={"/resume"}
-                    className="hover:text-accent group flex items-center transition-all duration-200"
-                  >
-                    <span>résumé</span>
-                  </Link>
-                </SheetClose>
-                <div>
-                  <ThemeToggle />
-                </div>
-              </div>
-          </SheetContent>
-        </Sheet>
+      <div className="sm:hidden">
+        seo-crawler
+      </div>
+      
+      <div>
+        <ThemeToggle />
       </div>
     </div>
   );
